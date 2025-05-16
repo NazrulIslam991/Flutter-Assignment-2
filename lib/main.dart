0a3ec19ddd93c31211690abfd5195598fbaf9d90
mdnaz <nazrulislamnayon991@gmail.com>
2025-05-16 19:25:56 +0600

    Assignment_2 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,204 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<Map<String, dynamic>> items = [
+    {
+      'image': 'bd_flag.png',
+      'name': 'Bangladesh',
+      'description': 'Rich culture, lush green landscapes.',
+    },
+    {
+      'image': 'afg_flag.png',
+      'name': 'Afghanistan',
+      'description': 'Mountainous terrain, resilient people.',
+    },
+    {
+      'image': 'aus_flag.png',
+      'name': 'Australia',
+      'description': 'Diverse wildlife, vast outback.',
+    },
+    {
+      'image': 'braz_flag.png',
+      'name': 'Brazil',
+      'description': 'Vibrant festivals, Amazon rainforest.',
+    },
+    {
+      'image': 'eng_flag.jpeg',
+      'name': 'England',
+      'description': 'Historic landmarks, royal heritage.',
+    },
+    {
+      'image': 'jap_flag.png',
+      'name': 'Japan',
+      'description': 'Advanced technology, traditional customs.',
+    },
+    {
+      'image': 'nz_flag.png',
+      'name': 'New Zealand',
+      'description': 'Stunning nature, Maori culture.',
+    },
+    {
+      'image': 'sa_flag.png',
+      'name': 'South Africa',
+      'description': 'Cultural diversity, wildlife safaris.',
+    },
+    {
+      'image': 'soudi_flag.png',
+      'name': 'Saudi Arabia',
+      'description': 'Desert landscapes, Islamic heritage.',
+    },
+    {
+      'image': 'zim_flag.png',
+      'name': 'Zimbabwe',
+      'description': 'Victoria Falls, rich wildlife.',
+    },
+    {
+      'image': 'bd_flag.png',
+      'name': 'Bangladesh',
+      'description': 'Rich culture, lush green landscapes.',
+    },
+    {
+      'image': 'bd_flag.png',
+      'name': 'Bangladesh',
+      'description': 'Rich culture, lush green landscapes.',
+    },
+    {
+      'image': 'bd_flag.png',
+      'name': 'Bangladesh',
+      'description': 'Rich culture, lush green landscapes.',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double _screenSize = MediaQuery.of(context).size.width;
+
+    int crossAxisCount;
+    double _childAspectRatio;
+
+    if (_screenSize >= 1024) {
+      crossAxisCount = 4;
+      _childAspectRatio = 1.2;
+    } else if (_screenSize >= 768 && _screenSize < 1024) {
+      crossAxisCount = 3;
+      _childAspectRatio = 1.0;
+    } else {
+      crossAxisCount = 2;
+      _childAspectRatio = 0.8;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "World Explorer",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: crossAxisCount,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: _childAspectRatio,
+          ),
+
+          itemCount: items.length,
+
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: Colors.grey[150],
+              elevation: 8,
+              shadowColor: Colors.blueAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10),
+                      ),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Image.asset(
+                          'images/${items[index]['image']}',
+                          height: 60,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          items[index]['name'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+
+                        SizedBox(height: 9),
+
+                        Text(
+                          items[index]['description'],
+                          style: TextStyle(fontSize: 12),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+
+                        TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "More Details",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Padding(padding: EdgeInsets.only(left: 5)),
+                              Icon(Icons.arrow_forward_rounded),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
